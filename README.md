6e230e4055649cbfbd2245f8b6c571b8327193ad
ecsalina <eric.salina@protonmail.com>
2023-11-10 21:00:12 -0500

    Update README

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ IPP version supported is IPP Everywhere.
 
 Based on guide https://www.pwg.org/ipp/ippguide.html and https://ftp.pwg.org/pub/pwg/candidates/cs-ippig20-20150821-5100.19.pdf, as well as the formal spec here: https://ftp.pwg.org/pub/pwg/candidates/cs-ippeve10-20130128-5100.14.pdf.
 
+
+Mostly an excuse to learn about the internet printing protocol and as a refresher on sockets and asynchronous python programming (instead of e.g. using an off-the-shelf python http server).
--- printer.py
+++ printer.py
@@ -7,7 +7,27 @@ from dataclasses import dataclass
 
 
 class Printer:
-    pass
+    def __init__(self):
+        pass
+
+    def receive_request(self: Printer, request: Request) -> None:
+#        match request.operation:
+#            case "Create-Job":
+#                self.create_job(request)
+#            case "Send-Document":
+#                self.send_document(request)
+#            case "Print-Job":
+#                self.print_job(request)
+#            case "Get-Printer-Attributes":
+#                self.get_printer_attributes(request)
+#            case "Get-Jobs":
+#                self.get_jobs(request)
+#            case "Get-Job-Attributes":
+#                self.get_job_attributes(request)
+#            case "Cancel-Job":
+#                self.cancel_job(request)
+        print(request) #DEBUG
+        return
 
 @dataclass
 class Request:
@@ -16,12 +36,21 @@ class Request:
     request_id: int
     groups: [Group]
     file_name: str
+    data: str
 
     def __init__(self, data=None):
         if not data:
             return
         else:
             _parse_request(data)
+
+    def _parse_request(data: str) -> None:
+        #TODO: implement parser
+        pass
+
+    def __str__(self: Request): -> str:
+        return(data)
+
 
 @dataclass
 class Group:
