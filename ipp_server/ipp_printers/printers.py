5b3a3742d84498a9a17388b566f68774afb8f9d9
ecsalina <eric.salina@protonmail.com>
2023-11-16 00:31:47 -0500

    move everything to printer.py and IppRequest class along with more rewrites

--- ipp_server/ipp_printers/printers.py
+++ ipp_server/ipp_printers/printers.py
@@ -17,27 +17,101 @@ class Response:
     pass
 
 
-@dataclass
 class IppRequest:
     version : str
     operation: str
     request_id: int
     groups: [Group]
     file_name: str
-    data: str
+    data: bytes
 
-    def __init__(self, data=None):
-        if not data:
-            return
+    def __init__(self, body: bytes | dict):
+        if type(body) is bytes:
+            body_dict = parse_requests(body)
         else:
-            _parse_request(data)
+            body_dict = body
 
-    def _parse_request(data: str) -> None:
-        #TODO: implement parser
-        pass
+        #prettify dict:
+        self.version = f"{body_dict['version_number_major']}.{body_dict['version_number_minor']}"
+#        self.operation = 
+        #etc.
 
-    def __str__(self) -> str:
-        return(data)
+    @classmethod
+    def parse_request(cls, body: bytes) -> dict:
+        """
+        Accepts the raw bytes from the POST body of an IPP request over HTTP(S)
+        and returns a dictionary containing the parsed components.
+        """
+	operation_attributes_tag = b'\x01'
+        job_attributes_tag = b'\x02'
+        end_of_attributes_tag = b'\x03'
+        printer_attributes_tag = b'\x04'
+        unsupported_attributes_tag = b'\x05'
+        
+        delimiters = (b'\x01', b'\x02', b'\x03', b'\x04', b'\x05') #same as above
+        
+        body = Stream(body)
+        
+        try:
+            version_number_major = int.from_bytes(body.pop(), signed=True)
+            version_number_minor = int.from_bytes(body.pop(), signed=True)
+            operation_id = int.from_bytes(body.pop(2), signed=True)
+            request_id = int.from_bytes(body.pop(4), signed=True)
+        
+            body_dict = {
+                    "version_number_major": version_number_major,
+                    "version_number_minor": version_number_minor,
+                    "operation_id": operation_id,
+                    "request_id":request_id,
+                    "attribute_groups": []}
+           
+            #actual decoding of POST body:
+            while body.get() != end_of_attributes_tag:
+                begin_attribute_group_tag = body.pop()
+                attribute_group = []
+                
+                #decode attributes repeatedly
+                while body.get() not in delimiters
+                    value_tag = body.pop()
+                    name_length = body.pop(2)
+                    name = body.pop(name_length)
+                    value_length = body.pop(2)
+                    value = body.pop(value_length)
+                    if name_length != 0: #this is an attribute-with-one-value
+                        attribute = [name, value]
+                        attribute_group[value_tag] = attribute
+                    else: #this is an additional-value 
+                        attribute.append(value)
+
+                body_dict["attribute_groups"][begin_attribute_group_tag] = attribute_group
+
+            data = body[9:] or b''
+            body_dict["data"] = data
+
+            return(body_dict)
+        except IndexError:
+            raise IppRequestParseError()
+
+class IppRequestParseError(Exception):
+    pass
+
+class Stream():
+    """
+    Auxiliary class for accessing the POST body bytes.
+    """
+    def __init__(self, data: bytes):
+        self.data = data
+        self.index = 0
+
+    def pop(size: int = 1):
+        """Returns next `size` element(s) and increments index"""
+        d = self.data[index:index+size]
+        index += size
+        return(d)
+
+    def get(size: int = 1):
+        """Returns next `size` element(s) without incrementing index"""
+        return(self.data[index:index+size)
 
 class Printer:
     def __init__(self):
--- ipp_server/ipp_printers/views.py
+++ ipp_server/ipp_printers/views.py
@@ -4,80 +4,11 @@ from django.utils.html import escape
 from . import printers
 
 def receive_request(request: HttpRequest):
-    ipp_request = decode_request(request.body)
     print(request.META) #DEBUG
     print(request.body) #DEBUG
-    return(HttpResponse(status=200))
-
-def decode_request(body: bytes) -> printers.IppRequest:
-    operation_attributes_tag = b'\x01'
-    job_attributes_tag = b'\02'
-    end_of_attributes_tag = b'\x03'
-    printer_attributes_tag = b'\x04'
-    unsupported_attributes_tag = b'\x05'
-
-    body = Stream(body)
-
     try:
-        version_number_major = int.from_bytes(body.pop(), signed=True)
-        version_number_minor = int.from_bytes(body.pop(), signed=True)
-        operation_id = int.from_bytes(body.pop(2), signed=True)
-        request_id = int.from_bytes(body.pop(4), signed=True)
-
-        prev_begin_attribute_group_tag = body.get()
-        
-        while body.get() != end_of_attributes_tag:
-            begin_attribute_group_tag = body.pop()
-            while begin_attribute_group_tag != prev_begin_attribute_group_tag:
-                #decode single attribute
-                #decode one value of attribute
-                while body.get() != end#################################
-                value_tag = body.pop()
-                name_length = body.pop(2)
-                name = body.pop(name_length)
-                value_length = body.pop(2)
-                value = body.pop(value_length)
-                #decode additional values of attribute
-                while body.get() == value_tag:  #as opposed to next attribute or attribute group
-                   value_tag = body.pop() #don't really need this as should be same
-                   name_length = 
-
-            #save old group and start new group
-            prev_begin_attribute_group_tag = begin_attribute_group_tag
-            index += 1
-                
-            
-
-
-            data = body[9:] or b''
-            return(printers.IppRequest()) #TODO fill in attributes
-    except IndexError:
-        raise IppRequestParseError()
-
-
-class IppRequestParseError(Exception):
-    pass
-
-
-class Stream():
-    def __init__(self, data: bytes):
-        self.data = data
-        self.index = 0
-
-    def pop(size: int = 1):
-        """Returns next `size` element(s) and increments index"""
-        d = self.data[index:index+size]
-        index += size
-        return(d)
-
-    def get(size: int = 1):
-        """Returns next `size` element(s) without incrementing index"""
-        return(self.data[index:index+size)
-
-
-
-
-
-
-
-
+        ipp_request = printers.IppRequest(request.body)
+    except:
+        return(HttpResponse(status=500)
+    else:
+        return(HttpResponse(status=200)
