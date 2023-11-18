964dc7dc5c2b710fc002d505f4ef2d7dc716286e
ecsalina <eric.salina@protonmail.com>
2023-11-18 18:29:15 -0500

    fix errors and replace tag byte codes with actual keywords. Parsing works!

--- ipp_server/ipp_printers/printers.py
+++ ipp_server/ipp_printers/printers.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
+from typing import Union
 
 @dataclass
 class Attribute:
     attr_type: str
-    key: str
+    name: str
     value: str
 
 
@@ -12,8 +13,8 @@ class Group:
     name: str
     attributes: [Attribute]
 
-@dataclass
-class Response:
+
+class IppResponse:
     pass
 
 
@@ -25,58 +26,107 @@ class IppRequest:
     file_name: str
     data: bytes
 
-    def __init__(self, body: bytes | dict):
+    def __init__(self, body: Union[bytes, dict]):
         if type(body) is bytes:
-            body_dict = parse_requests(body)
+            body_dict = self.parse_request(body)
         else:
             body_dict = body
 
+        import pprint #DEBUG
+        pprint.pprint(body_dict)  #DEBUG
+
         #prettify dict:
-        self.version = f"{body_dict['version_number_major']}.{body_dict['version_number_minor']}"
-#        self.operation = 
-        #etc.
+        #TODO etc.
 
     @classmethod
     def parse_request(cls, body: bytes) -> dict:
         """
-        Accepts the raw bytes from the POST body of an IPP request over HTTP(S)
-        and returns a dictionary containing the parsed components.
+        Accepts the raw bytes from the POST body of an IPP request over HTTP(S) and returns a
+        dictionary containing the parsed components in a human-readable format.
         """
-	operation_attributes_tag = b'\x01'
-        job_attributes_tag = b'\x02'
-        end_of_attributes_tag = b'\x03'
-        printer_attributes_tag = b'\x04'
-        unsupported_attributes_tag = b'\x05'
+
+        #GET THE DATA 
         
-        delimiters = (b'\x01', b'\x02', b'\x03', b'\x04', b'\x05') #same as above
+        delimiters = {
+                b'\x01': "operation-attributes-tag",
+                b'\x02': "job-attributes-tag" ,
+                b'\x03': "end-of-attributes-tag",
+                b'\x04': "printer-attributes-tag",
+                b'\x05': "unsupported-attributes-tag"}
+
+        end_of_attributes_tag = b'\x03' #already above but useful to have separate
+
+        value_tags = {
+                b'\x10': "unsupported",
+                b'\x12': "unknown",
+                b'\x13': "no-value",
+                b'\x21': "integer",
+                b'\x22': "boolean",
+                b'\x23': "enum",
+                b'\x31': "dateTime",
+                b'\x32': "resolution",
+                b'\x33': "rangeOfInteger",
+                b'\x34': "begCollection",
+                b'\x35': "textWithLanguage",
+                b'\x36': "nameWithLanguage",
+                b'\x37': "endCollection",
+                b'\x41': "textWithoutLanguage",
+                b'\x42': "nameWithoutLanguage",
+                b'\x44': "keyword",
+                b'\x45': "uri",
+                b'\x46': "uriScheme",
+                b'\x47': "charset",
+                b'\x48': "naturalLanguage",
+                b'\x49': "mimeMediaType",
+                b'\x4a': "memberAttrName"}
         
+        operation_ids = {
+                b'\x00\x02': "Print-Job",
+                b'\x00\x03': "Print-URI",
+                b'\x00\x04': "Validate-Job",
+                b'\x00\x05': "Create-Job",
+                b'\x00\x06': "Send-Document",
+                b'\x00\x07': "Send-URI",
+                b'\x00\x08': "Cancel-Job",
+                b'\x00\x09': "Get-Job-Attributes",
+                b'\x00\x0a': "Get-Jobs",
+                b'\x00\x0b': "Get-Printer-Attributes",
+                b'\x00\x0c': "Hold-Job",
+                b'\x00\x0d': "Release-Job",
+                b'\x00\x0e': "Restart-Job",
+                b'\x00\x10': "Pause-Printer",
+                b'\x00\x11': "Resume-Printer",
+                b'\x00\x12': "Purge-Jobs"}
+
+
+
         body = Stream(body)
         
         try:
-            version_number_major = int.from_bytes(body.pop(), signed=True)
-            version_number_minor = int.from_bytes(body.pop(), signed=True)
-            operation_id = int.from_bytes(body.pop(2), signed=True)
-            request_id = int.from_bytes(body.pop(4), signed=True)
+            version_number_major = int.from_bytes(body.pop(), byteorder="big", signed=True)
+            version_number_minor = int.from_bytes(body.pop(), byteorder="big", signed=True)
+            operation_id = operation_ids[body.pop(2)]
+            request_id = int.from_bytes(body.pop(4), byteorder="big", signed=True)
         
             body_dict = {
-                    "version_number_major": version_number_major,
-                    "version_number_minor": version_number_minor,
+                    "raw_bytes": body.all(),
+                    "version_number": float(f"{version_number_major}.{version_number_minor}"),
                     "operation_id": operation_id,
                     "request_id":request_id,
-                    "attribute_groups": []}
+                    "attribute_groups": {}}
            
             #actual decoding of POST body:
             while body.get() != end_of_attributes_tag:
-                begin_attribute_group_tag = body.pop()
-                attribute_group = []
+                begin_attribute_group_tag = delimiters[body.pop()]
+                attribute_group = {}
                 
                 #decode attributes repeatedly
-                while body.get() not in delimiters
-                    value_tag = body.pop()
-                    name_length = body.pop(2)
-                    name = body.pop(name_length)
-                    value_length = body.pop(2)
-                    value = body.pop(value_length)
+                while body.get() not in delimiters.keys():
+                    value_tag = value_tags[body.pop()]
+                    name_length = int.from_bytes(body.pop(2), byteorder="big", signed=True)
+                    name = body.pop(name_length).decode()
+                    value_length = int.from_bytes(body.pop(2), byteorder="big", signed=True)
+                    value = body.pop(value_length).decode()
                     if name_length != 0: #this is an attribute-with-one-value
                         attribute = [name, value]
                         attribute_group[value_tag] = attribute
@@ -84,16 +134,18 @@ class IppRequest:
                         attribute.append(value)
 
                 body_dict["attribute_groups"][begin_attribute_group_tag] = attribute_group
-
-            data = body[9:] or b''
-            body_dict["data"] = data
+            
+            #want to discard the first elt as it is the end_of_attributes_tag
+            body_dict["data"] = body.tail(1)
 
             return(body_dict)
         except IndexError:
             raise IppRequestParseError()
 
+
 class IppRequestParseError(Exception):
     pass
+
 
 class Stream():
     """
@@ -103,15 +155,29 @@ class Stream():
         self.data = data
         self.index = 0
 
-    def pop(size: int = 1):
-        """Returns next `size` element(s) and increments index"""
-        d = self.data[index:index+size]
-        index += size
+    def pop(self, size: int = 1):
+        """Returns next `size` element(s) and increments index.
+        If there are no more elements then the empty byte b'' is returned."""
+        d = self.data[self.index:self.index+size]
+        self.index += size
         return(d)
 
-    def get(size: int = 1):
-        """Returns next `size` element(s) without incrementing index"""
-        return(self.data[index:index+size)
+    def get(self, size: int = 1):
+        """Returns next `size` element(s) without incrementing index
+        If there are no more elements then the empty byte b'' is returned."""
+        return(self.data[self.index:self.index+size])
+
+    def tail(self, start=0):
+        """Returns the rest of the elements, leaving Stream empty.
+        If `start` is supplied, then discards the first `start` elements and returns the rest of
+        the elements.
+        If there are no more elements then the empty byte b'' is returned."""
+        self.pop(start)
+        return(self.data[self.index:])
+
+    def all(self):
+        """Returns the entire data Stream regardless of index, without incrementing the index."""
+        return(self.data)
 
 class Printer:
     def __init__(self):
--- ipp_server/ipp_printers/views.py
+++ ipp_server/ipp_printers/views.py
@@ -4,11 +4,11 @@ from django.utils.html import escape
 from . import printers
 
 def receive_request(request: HttpRequest):
-    print(request.META) #DEBUG
-    print(request.body) #DEBUG
-    try:
-        ipp_request = printers.IppRequest(request.body)
-    except:
-        return(HttpResponse(status=500)
-    else:
-        return(HttpResponse(status=200)
+#    try:
+#        ipp_request = printers.IppRequest(request.body)
+#    except:
+#        return(HttpResponse(status=500))
+#    else:
+#        return(HttpResponse(status=200))
+    ipp_request = printers.IppRequest(request.body)
+    return(HttpResponse(status=200))
